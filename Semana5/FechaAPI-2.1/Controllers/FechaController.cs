4e977d897bc45a9b8b74eab7cc286fc13339c303
IgnacioMaruccoTUP <412366@tecnicatura.frc.utn.edu.ar>
2024-09-11 10:18:13 -0300

    Add problema 2.1 FechaAPI

---
+++ Semana5/FechaAPI-2.1/Controllers/FechaController.cs
@@ -0,0 +1,17 @@
+﻿using FechaAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FechaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FechaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new Fecha()); //El objeto new Fecha() sera enviado como Response Body, con el codigo 200 (correcto, exitoso)
+        }
+    }
+}
---
+++ Semana5/FechaAPI-2.1/Models/Fecha.cs
@@ -0,0 +1,20 @@
+﻿namespace FechaAPI.Models
+{
+    public class Fecha
+    {
+        public int Numero { get; set; }
+        public string Dia { get; set; }
+        public int Mes { get; set; }
+        public int Anio { get; set; }
+
+        public Fecha()
+        {
+            DateTime fechaActual = DateTime.Today;
+
+            Numero = fechaActual.Day;
+            Dia = fechaActual.DayOfWeek.ToString();
+            Mes = fechaActual.Month;
+            Anio = fechaActual.Year;
+        }
+    }
+}
