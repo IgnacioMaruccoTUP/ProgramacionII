c7e63c945ba85233e595e06db834a7c94629c83c
IgnacioMaruccoTUP <412366@tecnicatura.frc.utn.edu.ar>
2024-09-11 23:57:31 -0300

    Add ProductoApp 2.4

---
+++ Semana5/ProductoApp/Services/IAplication.cs
@@ -0,0 +1,11 @@
+﻿using ProductoApp.Models;
+
+namespace ProductoApp.Services
+{
+    public interface IAplication
+    {
+        bool AddOrUpdateProduct(Product oProduct);
+        bool DeleteProduct(int code);
+        List<Product> GetProducts();
+    }
+}
---
+++ Semana5/ProductoApp/Services/ProductService.cs
@@ -0,0 +1,47 @@
+﻿using ProductoApp.Models;
+
+namespace ProductoApp.Services
+{
+    public class ProductService : IAplication
+    {
+        public static readonly List<Product> listaProductos = new List<Product>();
+
+        public List<Product> GetProducts()
+        {
+            return listaProductos;
+        }
+
+        public bool AddOrUpdateProduct(Product oProduct)
+        {
+
+            foreach (Product product in listaProductos)
+            {
+                if (oProduct.Code == product.Code)
+                {
+                    product.Name = oProduct.Name;
+                    product.Price = oProduct.Price;
+                    return true;
+                }
+            }
+
+            if (oProduct == null)
+                return false;
+
+            listaProductos.Add(oProduct);
+            return true;
+        }
+
+        public bool DeleteProduct(int code)
+        {
+            var product = listaProductos.Find(p =>  p.Code == code);
+
+            if (product == null)
+                return false;
+
+            listaProductos.Remove(product);
+            return true;
+
+
+        }            
+    }
+}
