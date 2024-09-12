3c303ac4ceaa048b2b24791228cf0ec46aea1609
IgnacioMaruccoTUP <412366@tecnicatura.frc.utn.edu.ar>
2024-09-12 10:03:52 -0300

    Add ProductAPI

---
+++ Semana5/ProductAPI-2.4/Services/IAplication.cs
@@ -0,0 +1,12 @@
+﻿using ProductAPI.Models;
+
+namespace ProductAPI.Services
+{
+    public interface IAplication
+    {
+        bool AddOrUpdateProduct(Product oProduct);
+        bool DeleteProduct(int code);
+        List<Product> GetProducts();
+        Product GetProduct(int code);
+    }
+}
